812f115c6cd6ebef0201d039daace53d1d0f1e96
natalia <natalia_eletro@hotmail.com>
2020-09-01 14:15:48 -0300

    Lock screen orientation (portrait)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:p3_guide/src/service_locator.dart';
 import 'package:p3_guide/src/ui/app.dart';
 
 main() async {
   setUpServiceLocator();
-  runApp(Persona3App());
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(Persona3App());
+  });
+
+  // runApp(Persona3App());
 }
